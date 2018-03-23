a2f74126f6222d513eaba8083a2e4b99024f3062
arimieandreea <arimie_andreea@yahoo.com>
2018-03-23 18:58:37 +0200

    Initial commit

---
+++ src/main/java/Lab2Test/App.java
@@ -0,0 +1,13 @@
+package Lab2Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
